b68ad15a355a420dfbc2529fba56137f208b73b2
liyilun <530625549@qq.com>
2019-10-27 23:27:36 +0800

    init

--- tes.py
+++ tes.py
@@ -4,9 +4,9 @@ from pylatex.utils import italic, NoEscape
 doc = Document(default_filepath='basic',
               documentclass='article')
 
-doc.packages.append(Package("graph1"))
-doc.preamble.append(Command('date'))
+# doc.packages.append(Package("graph1"))
+# doc.preamble.append(Command('date'))
 with doc.create(Section('A second section')):
     doc.append('Some text.')
-doc.generate_tex(filepath='1.tex')
-doc.generate_pdf(filepath="1.tex")
+doc.generate_tex()
+# doc.generate_pdf(filepath="basic.tex")
